be5684c0ecb7ea6568a6b02d9ce235dc195d7fb5
DrCFloyd <54326046+DrCFloyd@users.noreply.github.com>
2019-10-04 18:29:53 -0400

    Create 2016-11-04-hawthorne.m

---
+++ _posts/2016-11-04-hawthorne.m
@@ -0,0 +1,70 @@
+---
+layout: post
+title: Hawthorne's <em>A Wonderbook for Girls and Boys</em> (????, 1903)
+posted-by: Courtney Floyd
+---
+
+<h1>Hawthorne's <em>A Wonderbook for Girls and Boys</em> (????, 1903)</h1>
+
+<h2 style="text-align:left">This week, we have two editions of Nathaniel Hawthorne's <em>A Wonderbook for Girls & Boys </em>(or, <em>A Wonderbook for Boys and Girls</em> depending on the edition).
+
+<!--more-->
+
+<p>The first is an undated edition published by Donahue, Henneberry & Co. of Chicago, which was given to me by some close friends. It's a 6" x 4.5", clothbound volume with a beautiful floral design on the cloth and silver, floral stamping  along the inner edge and spine. It looks a lot like a gift book, but is rather plain on the interior. The binding and illustration style make me suspect that it's pre-1900.</p>
+
+<p>There is no title (or text of any kind) on the front cover, only on the spine. The spine also includes Hawthorne's last name and an abbreviated publisher's name.</p>
+
+<p>The text block is top edge gilt. The endpapers are plain, in the same paper as the rest of the text block.</p>
+
+<p>This edition includes a photoxylograph (photographically developed wood engraving) frontispiece portrait of Hawthorne:</p>
+
+<img class="alignnone size-large wp-image-2152" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4803.jpg?w=768" alt="img_4803" style="text-align:center"><br>
+
+<p>Look at that mustache:</p><br>
+<img class="aligncenter size-large wp-image-2156" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4819.jpg?w=768" alt="img_4819" style="text-align:center"> <br>
+
+<img class="aligncenter size-large wp-image-2156" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4819.jpg?w=768" alt="img_4819" style="text-align:center">
+
+<p>And here's a look at the title page, in black and red and gothic font:</p>
+
+<img class="alignnone size-full wp-image-2160" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4804.jpg" alt="img_4804" style="text-align:center">
+
+<p>The rest of the text is an older style Roman. I suspect it's stereotyped, because the top line of most pages is blotted and/or blurred. There are no other illustrations in the volume. From a 21st-century perspective, this does not look like a children's book.</p>
+
+<hr />
+
+<p>The second edition we're looking at today is something I picked up at a local thriftshop last week. It's a green cloth bound volume by J.M. Dent of London and E.P. Dutton of New York. It's dated 1903, with what looks like chromolithograph cover design:</p>
+
+<img class="alignnone size-full wp-image-2186" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4805.jpg" alt="img_4805" style="text-align:center">
+<br>
+
+<img class="alignnone size-full wp-image-2187" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4806.jpg" alt="img_4806" style="text-align:center">
+
+<p>There is no decoration on the back cover.</p>
+
+<p>This edition <em>screams </em>children's book, adorned as it is with a Pegasus and a Medusa head.  The top edge of this edition's text block is also gilt.</p>
+
+<p>I find it very interesting (and very cool) that this edition inverts the order of "girls" and "boys" in the title.</p>
+
+<p>The endpapers are plain, the paper is wove, and the illustrations are tipped in on glossy paper.</p>
+
+<p>The half-title page is plain, featuring a shortened version of the title. It is followed by a color halftone frontispiece, captioned "Perseus Cuts Off Medusa's Head." As you can see , the coloring is very muddy. This muddiness is consistent throughout the illustrations.</p>
+
+<img class="alignnone size-full wp-image-2201" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4809.jpg" alt="img_4809" style="text-align:center">
+
+<p>The title page is a chromolithograph, featuring an old fashioned font and a Greek-style scene. The illustrations throughout the volume emphasize the classical nature of Hawthorne's tales:</p>
+
+<img class="alignnone size-full wp-image-2202" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4810.jpg" alt="img_4810" style="text-align:center">
+<br>
+<img class="alignnone size-full wp-image-2203" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4811.jpg" alt="img_4811" style="text-align:center">
+
+<p>There are classical architecture headpieces throughout the volume, too. This seems like an odd illustration choice in a volume meant for children, as these headpieces are more seem more technical than decorative.</p>
+
+<img class="alignnone size-full wp-image-2204" src="https://courtneyafloyd.files.wordpress.com/2016/10/img_4812.jpg" alt="img_4812" style="text-align:center">
+
+<p>The text is crisp and dark throughout the volume, so I suspect it's letterpress. All in all, it's a lovely edition, muddy coloring notwithstanding.</p>
+
+It never ceased to amaze me how different editions can be, and how much binding, illustration, layout, and font can impact one's experience of the text. Having never (yet) read this book, I couldn't have told you based on a scan of the gift-book edition that it was classically themed. The second edition we discussed today really made that theme obvious.</h2>
+
+
+ 
